5a4698db268bf95cc426e4aff194df0c5edbda40
nathenpriyonggo <rpriyo.uic.edu>
2025-03-04 15:46:47 -0600

    Unenabled duplicate clicks on buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,54 @@ import { useState } from "react"
 
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations";
+
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
 
 function App() {
-  const [ activePlayer, setActivePlayer ] = useState('X');
+  const [ gameTurns, setGameTurns ] = useState([]);
 
-  function handleSelectSquare() {
-    setActivePlayer(() => activePlayer === 'X' ? 'O' : 'X');
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns(prevTurns => {
+
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer},
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard 
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
       </div>
-
-      LOG
+      <Log turns={gameTurns} />
     </main>
   )
 }
